840e68fcb0011924fb3dbc43766a621119e9c164
wheerh1 <wheerh1@student.op.ac.nz>
2025-08-26 12:02:29 +1200

    cant get image to load but everything else is fine

--- pokemon API stuff/main.js
+++ pokemon API stuff/main.js
@@ -22,12 +22,53 @@ async function getData() {
 
 //use java + loop to display the name and urls of the 10 pokemon
 function displayStuff(data){
+    const list = document.createElement("ul");
     for(const pokemon of data)
     {
-      const list = document.createElement('ul')
-      list.innerHTML = `<li> ${pokemon.name}</li>`;
-      document.body.appendChild(list)
+      const item = document.createElement("li");
+      item.innerText = pokemon.name;
+
+      //adding event listner
+      item.addEventListener("click", () => {
+        onePokemon(pokemon.url);
+      });
+
+      list.appendChild(item)
+      
+      
     }
+    document.body.appendChild(list);
 }
+
+//ads card to dsi[play ] clicked pokemon info
+
+async function onePokemon(url){
+  try {
+    const data = await fetch(url);
+    if (!data.ok) {
+      throw new Error(`Response status: ${data.status}`);
+    }
+    
+    const result = await data.json();
+    console.log(result.name);
+    displayIndividual(result)
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function displayIndividual(pokemon){
+  // document.createElement('div');
+  // dispatchEvent.innerHTML = `<h2>${pokemon.name}</h2>`;
+  // document.appendChild(div);
+  pokemonCard.innerHTML = `<h2>${pokemon.name}</h2>
+  <image src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}" alt="bugger">`;
+  //add image to string literal^
+}
+const pokemonCard = document.createElement('div');
+  document.body.appendChild(pokemonCard);
+;
+
+
 
 //add event listeer to EACH POKEMON and listen for a click
